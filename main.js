3b072e910e2e0a825789b415b6d9ffc7412eab2b
hata <hata@stylerecipe.co.jp>
2016-03-27 02:34:37 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// document.write("Hello webpack");
+
+// var print = require("./print");
+// print("Hello webpack");
+
+var Vue = require('vue');
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+});
